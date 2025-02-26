8a0b731a5407ae443dc9c7ce939694d6075e7734
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2025-02-26 15:39:58 -0300

    Adicionando set do peso e da altura

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -11,18 +11,18 @@ public class Clinica {
 		//Criação dos objetos do tipo paciente
 			Paciente p1 = new Paciente();
 			p1.nome = "Ana Maria";
-			p1.altura = 1.65;
-			p1.dataNascimento = LocalDate.of(2003, 07, 15);
-			p1.peso = 63.500;
+			p1.setAltura(1.65);
+			p1.dataNascimento = LocalDate.of(2003, 7, 29);
+			p1.setPeso(45);
 			p1.telefone = "(11)912345678";
 			p1.estaInternado = true;
 			
 			
 			Paciente p2 = new Paciente();
 			p2.nome = "Julio Cesar";
-			p2.altura = 1.75;
+			p2.setAltura(1.75);
 			p2.dataNascimento = LocalDate.of(2008, 05, 28);
-			p2.peso = 75.500;
+			p2.setPeso(72);
 			p2.telefone = "(11)986484451";
 			p2.estaInternado = false;
 			
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -6,10 +6,28 @@ public class Paciente {
 
 	public String nome;
 	public LocalDate dataNascimento;
-	public double peso;
-	public double altura;
+	private double peso;
+	private double altura;
 	public String telefone;
 	public boolean estaInternado;
+	
+	public void setAltura(double altura) {
+		if(altura>0){
+			this.altura = altura;
+		}else {
+			System.out.println("A altura do paciente " + nome + " está incorreta!");
+		}
+		
+		
+	}
+	
+		public void setPeso(double peso ) {
+			if(peso>=40) {
+				this.peso = peso;
+			}else {
+				System.out.println("O peso do paciente " + nome + " está incorreto!");
+			}
+		}
 	
 	public void calcularIdade() {
 		
